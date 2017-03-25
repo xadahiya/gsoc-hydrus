df525d53fbd77ba21881f3a65536c3168457a854
xadahiya <xadahiya@gmail.com>
2017-03-25 11:24:24 +0530

    Switched to flask due to messy orm mapping in django flask with mongodb initial setup

--- hydrus/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import University, Student
-
-admin.site.register(University)
-admin.site.register(Student)
--- hydrus/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from rest_framework import routers
-from .views import StudentViewSet, UniversityViewSet
-
-router = routers.DefaultRouter()
-router.register(r'students', StudentViewSet)
-router.register(r'universities', UniversityViewSet)
-
-urlpatterns = router.urls
