597b44587b870fbcf319381c47896f1f4bfa0924
xadahiya <xadahiya@gmail.com>
2017-04-01 01:05:13 +0530

    documentation loading fix 8

--- contexts.py
+++ contexts.py
@@ -7,7 +7,7 @@ def gen_entrypoint_context():
         {
             "@context": {
                 "hydra": "http://www.w3.org/ns/hydra/core#",
-                "vocab": "https://polar-peak-76271.herokuapp.com/coffeeshop-api/vocab#",
+                "vocab": "https://polar-peak-76271.herokuapp.com/coffeeshop-api/vocab",
                 "EntryPoint": "vocab#EntryPoint",
                 "products": {
                     "@id": "vocab#EntryPoint/products",
@@ -23,7 +23,7 @@ def gen_product_collection_context():
     return jsonify({
         "@context": {
             "hydra": "http://www.w3.org/ns/hydra/core#",
-            "vocab": "https://polar-peak-76271.herokuapp.com/coffeeshop-api/vocab#",
+            "vocab": "https://polar-peak-76271.herokuapp.com/coffeeshop-api/vocab",
             "ProductCollection": "vocab#ProductCollection",
             "members": "http://www.w3.org/ns/hydra/core#member"
         }
@@ -34,7 +34,7 @@ def gen_product_context():
     return jsonify({
         "@context": {
             "hydra": "http://www.w3.org/ns/hydra/core#",
-            "vocab": "https://polar-peak-76271.herokuapp.com/coffeeshop-api/vocab#",
+            "vocab": "https://polar-peak-76271.herokuapp.com/coffeeshop-api/vocab",
             "Product": "http://schema.org/Product",
             "name": "http://schema.org/name",
             "description": "http://schema.org/description",
